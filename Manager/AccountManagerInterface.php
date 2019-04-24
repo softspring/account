0eab405bf41060dead69b071011a387c9be0bdde
Javi H. Gil <javihgil@gmail.com>
2019-04-24 11:33:50 +0200

    Add AccountManagerInterface

---
+++ Manager/AccountManagerInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Softspring\Account\Manager;
+
+use Doctrine\ORM\EntityRepository;
+use Softspring\Account\Model\AccountInterface;
+
+interface AccountManagerInterface
+{
+    /**
+     * @return string
+     */
+    public function getClass(): string;
+
+    /**
+     * @return EntityRepository
+     */
+    public function getRepository(): EntityRepository;
+
+    /**
+     * @return AccountInterface
+     */
+    public function create(): AccountInterface;
+
+    /**
+     * @param AccountInterface $user
+     */
+    public function save(AccountInterface $user): void;
+}
